422629249366410ad200a3a803925edb11e9ce93
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-09-23 12:52:18 +0200

    bugfix

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -40,7 +40,7 @@ const Featured = ({ type, setGenre }) => {
       {type && (
         <div className="category">
           <span>{type === "movie" ? "Movies" : "Series"}</span>
-          <select name="genre" id="genre" onChange={() => handleGenre}>
+          <select name="genre" id="genre" onChange={handleGenre}>
             <option>Genre</option>
             <option value="adventure">Adventure</option>
             <option value="comedy">Comedy</option>
